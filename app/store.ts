30dbe869fe9b111218d509bca7c4e67bf3d28dd9
0xA-10 <tony@natel.li>
2025-07-10 01:36:21 -0700

    feat: dagre layout

--- app/store.ts
+++ app/store.ts
@@ -8,7 +8,9 @@ import {
 	addEdge,
 	applyNodeChanges,
 	applyEdgeChanges,
+	Position,
 } from "@xyflow/react";
+import dagre from "dagre";
 
 export type AppNode = Node;
 
@@ -36,10 +38,41 @@ const initialNodes = [
 ];
 const initialEdges = [{ id: "n1-n2", source: "n1", target: "n2" }];
 
+const nodeWidth = 172;
+const nodeHeight = 36;
+const dagreGraph = new dagre.graphlib.Graph();
+dagreGraph.setDefaultEdgeLabel(() => ({}));
+const getLayoutedElements = (nodes: AppNode[], edges: Edge[], direction = "TB") => {
+	const isHorizontal = direction === "LR";
+	dagreGraph.setGraph({ rankdir: direction });
+
+	nodes.forEach((node) => {
+		dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+	});
+
+	edges.forEach((edge) => {
+		dagreGraph.setEdge(edge.source, edge.target);
+	});
+
+	dagre.layout(dagreGraph);
+
+	nodes.forEach((node) => {
+		const nodeWithPosition = dagreGraph.node(node.id);
+		node.targetPosition = isHorizontal ? Position.Left : Position.Top;
+		node.sourcePosition = isHorizontal ? Position.Right : Position.Bottom;
+
+		node.position = {
+			x: nodeWithPosition.x - nodeWidth / 2,
+			y: nodeWithPosition.y - nodeHeight / 2,
+		};
+	});
+
+	return { nodes, edges };
+};
+
 const initialState: InitialAppState = {
 	level: 1,
-	nodes: initialNodes,
-	edges: initialEdges,
+	...getLayoutedElements(initialNodes, initialEdges),
 };
 
 const useStore = create<AppState>((set, get) => ({
