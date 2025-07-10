a77c15749b6a88a23f986d38030a5652a94d5665
0xA-10 <tony@natel.li>
2025-07-09 21:00:39 -0700

    feat: sidebar drag n drop

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,14 +8,12 @@ export default () => {
 	const [_, setType] = useDnD();
 
 	const onDragStart = (event, nodeType) => {
-		console.log("dragdnd", { event, nodeType });
 		setType(nodeType);
 		event.dataTransfer.effectAllowed = "move";
 	};
 
 	return (
 		<div className="z-10 fixed">
-			<div className="description">You can drag these nodes to the pane on the right.</div>
 			<div className="dndnode input" onDragStart={(event) => onDragStart(event, "input")} draggable>
 				Input Node
 			</div>
